10f99849bca99847b5f1489e342c0ed50df76c34
kangyeop <lky5697@naver.com>
2021-01-06 00:32:49 +0900

    feat: 게시글 작성 제출 버튼 작성

---
+++ src/components/atoms/SubmitButton/SubmitButton.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Button } from "./styles";
+
+interface IProps {
+    onClick: () => void;
+    canClick: boolean;
+    style?: React.CSSProperties;
+}
+
+const SubmitButton: React.FC<IProps> = ({ onClick, canClick, style, children }) => {
+    return (
+        <Button style={style} onClick={onClick} canClick={canClick}>
+            {children}
+        </Button>
+    );
+};
+
+export default SubmitButton;
