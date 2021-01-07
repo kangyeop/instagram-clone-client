1c5cbacaf181421cdc6ad62c2b65ca5f3de19b52
kangyeop <lky5697@naver.com>
2021-01-07 18:08:30 +0900

    feat: 댓글 영역 아이콘 컴포넌트 작성

---
+++ src/components/molecules/CommentIcon/styles.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    display: flex;
+`;
+
+export const IconContainer = styled.div`
+    padding: 8px;
+    cursor: pointer;
+`;
