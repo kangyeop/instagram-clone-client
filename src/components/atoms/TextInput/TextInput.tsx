97b8e47640e41b335c345bfe669c5dcde75e7485
kangyeop <lky5697@naver.com>
2021-01-07 11:57:27 +0900

    feat: Textinput Props 수정

--- src/components/atoms/TextInput/TextInput.tsx
+++ src/components/atoms/TextInput/TextInput.tsx
@@ -4,11 +4,14 @@ import { TextArea } from "./styles";
 interface IProps {
     style?: React.CSSProperties;
     placeholder?: string;
-    onChange: () => void;
+    // eslint-disable-next-line no-unused-vars
+    onChange: (e: React.ChangeEvent<HTMLTextAreaElement>) => void;
 }
 
 const TextInput: React.FC<IProps> = ({ style, onChange, placeholder }) => {
-    return <TextArea style={style} onChange={onChange} placeholder={placeholder} />;
+    return (
+        <TextArea style={style} onChange={(e) => onChange(e)} placeholder={placeholder} />
+    );
 };
 
 export default TextInput;
