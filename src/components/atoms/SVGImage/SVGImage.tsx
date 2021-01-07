d1894a7ee586bc1a2bf850f7414f7bf2f7d39948
kangyeop <lky5697@naver.com>
2021-01-07 18:04:37 +0900

    feat: SVG fill option 추가

--- src/components/atoms/SVGImage/SVGImage.tsx
+++ src/components/atoms/SVGImage/SVGImage.tsx
@@ -4,11 +4,12 @@ interface IProps {
     d: string;
     height: string;
     width: string;
+    fill?: string;
 }
 
-const SVGImage: React.FC<IProps> = ({ d, height, width }) => {
+const SVGImage: React.FC<IProps> = ({ d, height, width, fill }) => {
     return (
-        <svg height={height} width={width} viewBox="0 0 48 48">
+        <svg height={height} width={width} fill={fill} viewBox="0 0 48 48">
             <path d={d} />
         </svg>
     );
