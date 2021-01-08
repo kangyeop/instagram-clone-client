dee4249e6729dbe8ab38c812ffc9f7848649e8dc
kangyeop <lky5697@naver.com>
2021-01-08 16:59:33 +0900

    feat: 게시글 본문 컴포넌트 작성

---
+++ src/components/molecules/PostContent/styles.ts
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    display: flex;
+`;
+
+export const ContentText = styled.div`
+    margin: 0;
+    margin-left: 14px;
+
+    font-size: 14px;
+    line-height: 22px;
+
+    line-break: anywhere;
+`;
