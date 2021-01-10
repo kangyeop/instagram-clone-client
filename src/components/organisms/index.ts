9a5a624b900d619aee4d20b321b5f8c087f8e4fb
kangyeop <lky5697@naver.com>
2021-01-10 13:57:24 +0900

    fix: Organisms index.ts 수정

--- src/components/organisms/index.ts
+++ src/components/organisms/index.ts
@@ -1,6 +1,5 @@
 import Header from "./Header";
 import PostCard from "./PostCard";
 import PostForm from "./PostForm";
-import FeedPostCard from "./FeedPostCard";
 
-export { Header, PostCard, PostForm, FeedPostCard };
+export { Header, PostCard, PostForm };
