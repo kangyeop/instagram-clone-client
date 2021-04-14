a2010afac0748e54faefc62ba0ad80f757cbe4fe
HyoungDong <52812307+HyoungDong@users.noreply.github.com>
2021-04-14 17:06:57 +0900

    JWT토큰 방식 대신 Session방식으로 로그인, 회원가입 과정을 수행하도록 수정

--- src/api/url.ts
+++ src/api/url.ts
@@ -1,3 +1,3 @@
-export const GithubLoginRequest = `${process.env.REACT_APP_API_URL}oauth2/authorization/github?finalRedirectUri=${process.env.REACT_APP_DOMAIN}/loginloading`;
-export const GoogleLoginRequest = `${process.env.REACT_APP_API_URL}oauth2/authorization/google?finalRedirectUri=${process.env.REACT_APP_DOMAIN}/loginloading`;
+export const GithubLoginRequest = `${process.env.REACT_APP_API_URL}oauth2/authorization/github?successRedirectUri=${process.env.REACT_APP_DOMAIN}loginLoading&signupRedirectUri=${process.env.REACT_APP_DOMAIN}signup`;
+export const GoogleLoginRequest = `${process.env.REACT_APP_API_URL}oauth2/authorization/google?successRedirectUri=${process.env.REACT_APP_DOMAIN}loginLoading&signupRedirectUri=${process.env.REACT_APP_DOMAIN}signup`;
 export const SignUpRequest = `${process.env.REACT_APP_API_URL}api/v1/members/signup`;
